dbaf2adf9c7d3ea7f7c49e451cc9aaacc8df62cb
Mihai Negrisan <mihainegrisan1@gmail.com>
2025-08-24 20:06:14 +0300

    Fixes #2 Add create and update DTOs

---
+++ src/MFO.CatalogService.Application/DTOs/Product/CreateProductDto.cs
@@ -0,0 +1,15 @@
+﻿namespace MFO.CatalogService.Application.DTOs.Product;
+
+public sealed record CreateProductDto
+{
+    public required string Name { get; init; }
+
+    public required decimal Price { get; init; }
+
+    public string? Description { get; init; }
+
+    public required Guid CategoryId { get; init; }
+
+    public required Guid BrandId { get; init; }
+}
+
