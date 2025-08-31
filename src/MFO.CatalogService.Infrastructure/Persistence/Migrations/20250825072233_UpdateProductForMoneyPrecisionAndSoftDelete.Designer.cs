2a50f5b0a40bd688f8c46166053aa92210348e2c
Mihai Negrisan <mihainegrisan1@gmail.com>
2025-08-31 17:12:16 +0300

    Run migrations

--- src/MFO.CatalogService.Infrastructure/Persistence/Migrations/20250825072233_UpdateProductForMoneyPrecisionAndSoftDelete.Designer.cs
+++ src/MFO.CatalogService.Infrastructure/Persistence/Migrations/20250825072233_UpdateProductForMoneyPrecisionAndSoftDelete.Designer.cs
@@ -4,6 +4,7 @@ using MFO.CatalogService.Infrastructure.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace MFO.CatalogService.Infrastructure.Migrations
 {
     [DbContext(typeof(CatalogDbContext))]
-    partial class CatalogDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250825072233_UpdateProductForMoneyPrecisionAndSoftDelete")]
+    partial class UpdateProductForMoneyPrecisionAndSoftDelete
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/MFO.CatalogService.Infrastructure/Persistence/Migrations/20250825072233_UpdateProductForMoneyPrecisionAndSoftDelete.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MFO.CatalogService.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateProductForMoneyPrecisionAndSoftDelete : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Price",
+                table: "Products",
+                type: "decimal(18,2)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsActive",
+                table: "Products",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsActive",
+                table: "Products");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Price",
+                table: "Products",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)");
+        }
+    }
+}
