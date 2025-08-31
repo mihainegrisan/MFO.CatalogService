45a2379b403cfafffb07e7341e662f23c7f4ee43
Mihai Negrisan <mihainegrisan1@gmail.com>
2025-08-31 17:14:50 +0300

    Add mapping

---
+++ src/MFO.CatalogService.Application/Common/Mapping/CatalogServiceProfile.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using MFO.CatalogService.Application.DTOs.Brand;
+using MFO.CatalogService.Application.DTOs.Category;
+using MFO.CatalogService.Application.DTOs.Product;
+using MFO.CatalogService.Domain.Entities;
+
+namespace MFO.CatalogService.Application.Common.Mapping;
+
+public class CatalogServiceProfile : Profile
+{
+    public CatalogServiceProfile()
+    {
+        // Entity → DTO
+        CreateMap<Product, GetProductDto>();
+        CreateMap<Category, GetCategoryDto>();
+        CreateMap<Brand, GetBrandDto>();
+
+        // DTO → Entity(for create / update)
+        CreateMap<CreateProductDto, Product>();
+        //CreateMap<UpdateProductDto, Product>();
+    }
+}
+
