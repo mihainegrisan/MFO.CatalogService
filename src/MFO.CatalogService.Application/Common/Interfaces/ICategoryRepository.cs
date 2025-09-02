8655edee4203b9d2df8097b5dff0c00ca514f989
Mihai Negrisan <mihainegrisan1@gmail.com>
2025-09-02 21:19:07 +0300

    Add ICategoryRepository

---
+++ src/MFO.CatalogService.Application/Common/Interfaces/ICategoryRepository.cs
@@ -0,0 +1,16 @@
+﻿using MFO.CatalogService.Domain.Entities;
+
+namespace MFO.CatalogService.Application.Common.Interfaces;
+
+public interface ICategoryRepository
+{
+    Task<Category?> GetCategoryByIdAsync(int id, CancellationToken cancellationToken);
+
+    Task<List<Category>> GetAllCategoriesAsync(CancellationToken cancellationToken);
+
+    Task<Category> AddCategoryAsync(Category category, CancellationToken cancellationToken);
+
+    Task<Category> UpdateCategoryAsync(Category category, CancellationToken cancellationToken);
+
+    Task<bool> DeleteCategoryAsync(Category category, CancellationToken cancellationToken);
+}
