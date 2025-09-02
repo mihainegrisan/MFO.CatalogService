f047570453aeb5eb8035c0717f7570255f158e3f
Mihai Negrisan <mihainegrisan1@gmail.com>
2025-09-02 21:20:21 +0300

    Fix #11 - Add CategoryRepository

---
+++ src/MFO.CatalogService.Infrastructure/Repositories/CategoryRepository.cs
@@ -0,0 +1,43 @@
+﻿using MFO.CatalogService.Application.Common.Interfaces;
+using MFO.CatalogService.Domain.Entities;
+using MFO.CatalogService.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+
+namespace MFO.CatalogService.Infrastructure.Repositories;
+
+public class CategoryRepository : ICategoryRepository
+{
+    private readonly CatalogDbContext _db;
+
+    public CategoryRepository(CatalogDbContext db)
+    {
+        _db = db;
+    }
+
+    public async Task<Category?> GetCategoryByIdAsync(int id, CancellationToken cancellationToken)
+        => await _db.Categories.FindAsync([id], cancellationToken);
+
+    public async Task<List<Category>> GetAllCategoriesAsync(CancellationToken cancellationToken)
+        => await _db.Categories.ToListAsync(cancellationToken);
+
+    public async Task<Category> AddCategoryAsync(Category category, CancellationToken cancellationToken)
+    {
+        await _db.Categories.AddAsync(category, cancellationToken);
+        await _db.SaveChangesAsync(cancellationToken);
+        return category;
+    }
+
+    public async Task<Category> UpdateCategoryAsync(Category category, CancellationToken cancellationToken)
+    {
+        _db.Categories.Update(category);
+        await _db.SaveChangesAsync(cancellationToken);
+        return category;
+    }
+
+    public async Task<bool> DeleteCategoryAsync(Category category, CancellationToken cancellationToken)
+    {
+        _db.Categories.Remove(category);
+        await _db.SaveChangesAsync(cancellationToken);
+        return true;
+    }
+}
