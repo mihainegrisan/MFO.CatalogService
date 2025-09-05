a90836ee01013d0a45e89ba9d2706edd9ccee389
Mihai Negrisan <mihainegrisan1@gmail.com>
2025-09-05 10:51:34 +0300

    Fix #7 - Add GetAll queries and handlers for all entities

---
+++ src/MFO.CatalogService.Application/Features/Brand/Queries/GetAllBrands/GetAllBrandsQueryHandler.cs
@@ -0,0 +1,36 @@
+﻿using AutoMapper;
+using FluentResults;
+using MediatR;
+using MFO.CatalogService.Application.Common.Interfaces;
+using MFO.CatalogService.Application.DTOs.Brand;
+
+namespace MFO.CatalogService.Application.Features.Brand.Queries.GetAllBrands;
+
+public sealed record GetAllBrandsQuery : IRequest<Result<IReadOnlyList<GetBrandDto>>>;
+
+public class GetAllBrandsQueryHandler : IRequestHandler<GetAllBrandsQuery, Result<IReadOnlyList<GetBrandDto>>>
+{
+    private readonly IBrandRepository _brandRepository;
+    private readonly IMapper _mapper;
+
+    public GetAllBrandsQueryHandler(IBrandRepository brandRepository, IMapper mapper)
+    {
+        _brandRepository = brandRepository;
+        _mapper = mapper;
+    }
+
+    public async Task<Result<IReadOnlyList<GetBrandDto>>> Handle(GetAllBrandsQuery request, CancellationToken cancellationToken)
+    {
+        var brands = await _brandRepository.GetAllBrandsAsync(cancellationToken);
+        if (brands.Count is 0)
+        {
+            return Result.Ok<IReadOnlyList<GetBrandDto>>(new List<GetBrandDto>());
+        }
+
+        var brandsDto = brands
+            .Select(brand => _mapper.Map<GetBrandDto>(brand))
+            .ToList();
+
+        return Result.Ok<IReadOnlyList<GetBrandDto>>(brandsDto);
+    }
+}
---
+++ src/MFO.CatalogService.Application/Features/Products/Queries/GetAllProducts/GetAllProductsQueryHandler.cs
@@ -0,0 +1,36 @@
+﻿using AutoMapper;
+using FluentResults;
+using MediatR;
+using MFO.CatalogService.Application.Common.Interfaces;
+using MFO.CatalogService.Application.DTOs.Product;
+
+namespace MFO.CatalogService.Application.Features.Products.Queries.GetAllProducts;
+
+public sealed record GetAllProductsQuery : IRequest<Result<IReadOnlyList<GetProductDto>>>;
+
+public class GetAllProductsQueryHandler : IRequestHandler<GetAllProductsQuery, Result<IReadOnlyList<GetProductDto>>>
+{
+    private readonly IProductRepository _productRepository;
+    private readonly IMapper _mapper;
+
+    public GetAllProductsQueryHandler(IProductRepository productRepository, IMapper mapper)
+    {
+        _productRepository = productRepository;
+        _mapper = mapper;
+    }
+
+    public async Task<Result<IReadOnlyList<GetProductDto>>> Handle(GetAllProductsQuery request, CancellationToken cancellationToken)
+    {
+        var products = await _productRepository.GetAllProductsAsync(cancellationToken);
+        if (products.Count is 0)
+        {
+            return Result.Ok<IReadOnlyList<GetProductDto>>(new List<GetProductDto>());
+        }
+
+        var productsDto = products
+            .Select(product => _mapper.Map<GetProductDto>(product))
+            .ToList();
+
+        return Result.Ok<IReadOnlyList<GetProductDto>>(productsDto);
+    }
+}
