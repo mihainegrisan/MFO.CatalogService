79e75f2962212e47bb8a228cd8b5dfebc747184b
Mihai Negrisan <mihainegrisan1@gmail.com>
2025-09-06 14:57:46 +0300

    Add indexes

--- src/MFO.CatalogService.Infrastructure/Persistence/CatalogDbContext.cs
+++ src/MFO.CatalogService.Infrastructure/Persistence/CatalogDbContext.cs
@@ -11,6 +11,24 @@ public class CatalogDbContext : DbContext
 
     public CatalogDbContext(DbContextOptions<CatalogDbContext> options) : base(options)
     {
-        
+
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Product>()
+            .HasIndex(p => p.SKU)
+            .IsUnique()
+            .HasDatabaseName("IX_Product_SKU");
+
+        modelBuilder.Entity<Category>()
+            .HasIndex(c => c.Name)
+            .IsUnique()
+            .HasDatabaseName("IX_Category_Name");
+
+        modelBuilder.Entity<Brand>()
+            .HasIndex(b => b.Name)
+            .IsUnique()
+            .HasDatabaseName("IX_Brand_Name");
     }
 }
