dc30d55bf40ef3f7f1e71f13537892b7501fc244
Mihai Negrisan <mihainegrisan1@gmail.com>
2025-09-07 17:57:50 +0300

    Add migrations - SKU and indexes

--- src/MFO.CatalogService.Infrastructure/Persistence/Migrations/20250906115604_Add_SKU_to_Product.Designer.cs
+++ src/MFO.CatalogService.Infrastructure/Persistence/Migrations/20250906115604_Add_SKU_to_Product.Designer.cs
@@ -4,6 +4,7 @@ using MFO.CatalogService.Infrastructure.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace MFO.CatalogService.Infrastructure.Migrations
 {
     [DbContext(typeof(CatalogDbContext))]
-    partial class CatalogDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250906115604_Add_SKU_to_Product")]
+    partial class Add_SKU_to_Product
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
