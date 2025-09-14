3df91fee6171d2126b64d1f947e26ca1196747c3
Mihai Negrisan <mihainegrisan1@gmail.com>
2025-09-14 16:24:39 +0300

    Fix #33 - Add company entity and DTOs

---
+++ src/MFO.CatalogService.Domain/Entities/Company.cs
@@ -0,0 +1,16 @@
+﻿using MFO.CatalogService.Domain.Common;
+
+namespace MFO.CatalogService.Domain.Entities;
+
+public class Company : AuditableEntity
+{
+    public required Guid CompanyId { get; set; }
+
+    public required string Name { get; set; }
+
+    public required string Code { get; set; }
+
+    public string? Description { get; set; } = null;
+
+    public bool IsActive { get; set; }
+}
