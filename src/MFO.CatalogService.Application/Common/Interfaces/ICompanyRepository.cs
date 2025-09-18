abd9538cbef98bdc6eb5cd98e7c7a6e93ba330c8
Mihai Negrisan <mihainegrisan1@gmail.com>
2025-09-18 17:39:11 +0300

    Add CompanyRepository and interface

---
+++ src/MFO.CatalogService.Application/Common/Interfaces/ICompanyRepository.cs
@@ -0,0 +1,18 @@
+﻿using MFO.CatalogService.Domain.Entities;
+
+namespace MFO.CatalogService.Application.Common.Interfaces;
+
+public interface ICompanyRepository
+{
+    Task<Company?> GetCompanyByIdAsync(Guid companyId, CancellationToken cancellationToken);
+
+    Task<IReadOnlyList<Company>> GetAllCompaniesAsync(CancellationToken cancellationToken);
+
+    Task<Company> AddCompanyAsync(Company company, CancellationToken cancellationToken);
+
+    Task<Company> UpdateCompanyAsync(Company company, CancellationToken cancellationToken);
+
+    Task<Company> SetCompanyActiveStateAsync(Company company, bool isActive, CancellationToken cancellationToken);
+
+    Task<bool> DeleteCompanyAsync(Company company, CancellationToken cancellationToken);
+}
---
+++ src/MFO.CatalogService.Infrastructure/Repositories/CompanyRepository.cs
@@ -0,0 +1,54 @@
+﻿using MFO.CatalogService.Application.Common.Interfaces;
+using MFO.CatalogService.Domain.Entities;
+using MFO.CatalogService.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+
+namespace MFO.CatalogService.Infrastructure.Repositories;
+
+public class CompanyRepository : ICompanyRepository
+{
+    private readonly CatalogDbContext _db;
+
+    public CompanyRepository(CatalogDbContext db)
+    {
+        _db = db;
+    }
+
+    public async Task<Company?> GetCompanyByIdAsync(Guid companyId, CancellationToken cancellationToken)
+        => await _db.Companies
+            .FindAsync([companyId], cancellationToken);
+
+    public async Task<IReadOnlyList<Company>> GetAllCompaniesAsync(CancellationToken cancellationToken)
+        => await _db.Companies
+            .AsNoTracking()
+            .ToListAsync(cancellationToken);
+
+    public async Task<Company> AddCompanyAsync(Company company, CancellationToken cancellationToken)
+    {
+        await _db.Companies.AddAsync(company, cancellationToken);
+        await _db.SaveChangesAsync(cancellationToken);
+        return company;
+    }
+
+    public async Task<Company> UpdateCompanyAsync(Company company, CancellationToken cancellationToken)
+    {
+        _db.Companies.Update(company);
+        await _db.SaveChangesAsync(cancellationToken);
+        return company;
+    }
+
+    public async Task<Company> SetCompanyActiveStateAsync(Company company, bool isActive, CancellationToken cancellationToken)
+    {
+        company.IsActive = isActive;
+        _db.Companies.Update(company);
+        await _db.SaveChangesAsync(cancellationToken);
+        return company;
+    }
+
+    public async Task<bool> DeleteCompanyAsync(Company company, CancellationToken cancellationToken)
+    {
+        _db.Companies.Remove(company);
+        await _db.SaveChangesAsync(cancellationToken);
+        return true;
+    }
+}
