4320c84be4795bf5a0804d8ab8e383142ca80617
Mihai Negrisan <mihainegrisan1@gmail.com>
2025-09-19 14:57:06 +0300

    Add Company CommandHandlers

---
+++ src/MFO.CatalogService.Application/Features/Companies/Commands/CreateCompanyCommand/CreateCompanyCommandHandler.cs
@@ -0,0 +1,39 @@
+﻿using AutoMapper;
+using FluentResults;
+using MediatR;
+using MFO.CatalogService.Application.Common.Interfaces;
+using MFO.CatalogService.Application.DTOs.Company;
+using MFO.CatalogService.Domain.Entities;
+
+namespace MFO.CatalogService.Application.Features.Companies.Commands.CreateCompanyCommand;
+
+public sealed record CreateCompanyCommand(CreateCompanyDto CreateCompanyDto) : IRequest<Result<GetCompanyDto>>;
+
+public class CreateCompanyCommandHandler : IRequestHandler<CreateCompanyCommand, Result<GetCompanyDto>>
+{
+    private readonly ICompanyRepository _companyRepository;
+    private readonly IMapper _mapper;
+
+    public CreateCompanyCommandHandler(ICompanyRepository companyRepository, IMapper mapper)
+    {
+        _companyRepository = companyRepository;
+        _mapper = mapper;
+    }
+
+    public async Task<Result<GetCompanyDto>> Handle(CreateCompanyCommand request, CancellationToken cancellationToken)
+    {
+        var company = _mapper.Map<Company>(request.CreateCompanyDto);
+        company.CompanyId = Guid.CreateVersion7();
+        company.IsActive = true;
+        company.CreatedBy = "system";
+        company.CreatedDate = DateTime.UtcNow;
+        company.LastModifiedBy = "system";
+        company.LastModifiedDate = DateTime.UtcNow;
+
+        await _companyRepository.AddCompanyAsync(company, cancellationToken);
+
+        var companyDto = _mapper.Map<GetCompanyDto>(company);
+
+        return Result.Ok(companyDto);
+    }
+}
