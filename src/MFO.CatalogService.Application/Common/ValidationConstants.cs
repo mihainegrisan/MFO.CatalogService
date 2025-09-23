d8de97b357f004673bdc2e82835764137fd7ee8b
Mihai Negrisan <mihainegrisan1@gmail.com>
2025-09-23 16:26:16 +0300

    Create ValidationConstants

---
+++ src/MFO.CatalogService.Application/Common/ValidationConstants.cs
@@ -0,0 +1,9 @@
+﻿namespace MFO.CatalogService.Application.Common;
+
+public static class ValidationConstants
+{
+    public const int NameMaxLength = 50;
+    public const int DescriptionMaxLength = 500;
+
+    public const int CodeLength = 5;
+}
