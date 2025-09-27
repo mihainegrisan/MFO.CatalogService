a0763dccdea39e2adede57194f6a41ffe42b5645
Mihai Negrisan <mihainegrisan1@gmail.com>
2025-09-27 14:43:07 +0300

    Fix #49 Add Commands for Company

---
+++ src/MFO.CatalogService.Application/Features/Companies/Commands/SetCompanyActiveState/SetCompanyActiveStateCommandHandler.cs
@@ -0,0 +1,35 @@
+﻿using AutoMapper;
+using FluentResults;
+using MediatR;
+using MFO.CatalogService.Application.Common.Interfaces;
+using MFO.CatalogService.Application.DTOs.Company;
+using MFO.CatalogService.Domain.Errors;
+
+namespace MFO.CatalogService.Application.Features.Companies.Commands.SetCompanyActiveState;
+
+public sealed record SetCompanyActiveStateCommand(Guid CompanyId, bool IsActive) : IRequest<Result<GetCompanyDto>>;
+
+public class SetCompanyActiveStateCommandHandler : IRequestHandler<SetCompanyActiveStateCommand, Result<GetCompanyDto>
+{
+    private readonly ICompanyRepository _companyRepository;
+    private readonly IMapper _mapper;
+
+    public SetCompanyActiveStateCommandHandler(ICompanyRepository companyRepository, IMapper mapper)
+    {
+        _companyRepository = companyRepository;
+        _mapper = mapper;
+    }
+
+    public async Task<Result<GetCompanyDto>> Handle(SetCompanyActiveStateCommand request, CancellationToken cancellationToken)
+    {
+        var company = await _companyRepository.SetCompanyActiveStateAsync(request.CompanyId, request.IsActive, cancellationToken);
+        if (company is null)
+        {
+            return Result.Fail(new NotFoundError($"Company with ID {request.CompanyId} was not found."));
+        }
+
+        var companyDto = _mapper.Map<GetCompanyDto>(company);
+
+        return Result.Ok(companyDto);
+    }
+}
