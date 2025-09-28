58119c440ba9b860663ac876ed8c579e95fee194
Mihai Negrisan <mihainegrisan1@gmail.com>
2025-09-28 11:30:28 +0300

    Add delete handlers

---
+++ src/MFO.CatalogService.Application/Features/Brand/Commands/DeleteBrand/DeleteBrandCommandHandler.cs
@@ -0,0 +1,30 @@
+﻿using FluentResults;
+using MediatR;
+using MFO.CatalogService.Application.Common.Interfaces;
+using MFO.CatalogService.Domain.Errors;
+
+namespace MFO.CatalogService.Application.Features.Brand.Commands.DeleteBrand;
+
+public sealed record DeleteBrandCommand(Guid BrandId) : IRequest<Result>;
+
+public class DeleteBrandCommandHandler : IRequestHandler<DeleteBrandCommand, Result>
+{
+    private readonly IBrandRepository _brandRepository;
+
+    public DeleteBrandCommandHandler(IBrandRepository brandRepository)
+    {
+        _brandRepository = brandRepository;
+    }
+
+    public async Task<Result> Handle(DeleteBrandCommand request, CancellationToken cancellationToken)
+    {
+        var ok = await _brandRepository.DeleteBrandAsync(request.BrandId, cancellationToken);
+
+        if (!ok)
+        {
+            return Result.Fail(new NotFoundError($"Brand with ID {request.BrandId} was not found."));
+        }
+
+        return Result.Ok();
+    }
+}
---
+++ src/MFO.CatalogService.Application/Features/Products/Commands/DeleteProduct/DeleteProductCommandHandler.cs
@@ -0,0 +1,30 @@
+﻿using FluentResults;
+using MediatR;
+using MFO.CatalogService.Application.Common.Interfaces;
+using MFO.CatalogService.Domain.Errors;
+
+namespace MFO.CatalogService.Application.Features.Products.Commands.DeleteProduct;
+
+public sealed record DeleteProductCommand(Guid ProductId) : IRequest<Result>;
+
+public class DeleteProductCommandHandler : IRequestHandler<DeleteProductCommand, Result>
+{
+    private readonly IProductRepository _productRepository;
+
+    public DeleteProductCommandHandler(IProductRepository productRepository)
+    {
+        _productRepository = productRepository;
+    }
+
+    public async Task<Result> Handle(DeleteProductCommand request, CancellationToken cancellationToken)
+    {
+        var ok = await _productRepository.DeleteProductAsync(request.ProductId, cancellationToken);
+
+        if (!ok)
+        {
+            return Result.Fail(new NotFoundError($"Product with ID {request.ProductId} was not found."));
+        }
+
+        return Result.Ok();
+    }
+}
