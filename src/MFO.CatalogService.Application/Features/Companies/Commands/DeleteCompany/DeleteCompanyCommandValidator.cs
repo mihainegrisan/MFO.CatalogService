c2294ac9c49ea53379e37cc2a6062fe54decf890
Mihai Negrisan <mihainegrisan1@gmail.com>
2025-09-28 11:26:19 +0300

    Fix #36 Validation for Company Queries and Commands

---
+++ src/MFO.CatalogService.Application/Features/Companies/Commands/DeleteCompany/DeleteCompanyCommandValidator.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+
+namespace MFO.CatalogService.Application.Features.Companies.Commands.DeleteCompany;
+
+public class DeleteCompanyCommandValidator : AbstractValidator<DeleteCompanyCommand>
+{
+    public DeleteCompanyCommandValidator()
+    {
+        RuleFor(c => c.CompanyId)
+            .NotEmpty().WithMessage("CompanyId is required.");
+    }
+}
---
+++ src/MFO.CatalogService.Application/Features/Companies/Commands/SetCompanyActiveState/SetCompanyActiveStateCommandValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+
+namespace MFO.CatalogService.Application.Features.Companies.Commands.SetCompanyActiveState;
+
+public class SetCompanyActiveStateCommandValidator : AbstractValidator<SetCompanyActiveStateCommand>
+{
+    public SetCompanyActiveStateCommandValidator()
+    {
+        RuleFor(c => c.CompanyId)
+            .NotEmpty().WithMessage("CompanyId is required.");
+
+        RuleFor(c => c.IsActive)
+            .NotEmpty().WithMessage("IsActive is required.");
+    }
+}
