f02e962aa5e3b44826a9c7b2cfe41bafd1960c1a
Mihai Negrisan <mihainegrisan1@gmail.com>
2025-10-01 10:29:46 +0300

    Fix #46 Create SkuRepository and interface

---
+++ src/MFO.CatalogService.Application/Common/Interfaces/ISkuSequenceRepository.cs
@@ -0,0 +1,6 @@
+﻿namespace MFO.CatalogService.Application.Common.Interfaces;
+
+public interface ISkuSequenceRepository
+{
+    Task<int> GetNextNumberForSkuAsync(string company, string category, string brand, CancellationToken cancellationToken);
+}
