b45711b5a9c782738a5ad1fe95f548e1a1e6d6e7
Mihai Negrisan <mihainegrisan1@gmail.com>
2025-10-01 10:33:37 +0300

    Create SkuGenerator and interface

---
+++ src/MFO.CatalogService.Application/Services/SkuGenerator.cs
@@ -0,0 +1,23 @@
+﻿using MFO.CatalogService.Application.Common.Interfaces;
+
+namespace MFO.CatalogService.Application.Services;
+
+public class SkuGenerator : ISkuGenerator
+{
+    private readonly ISkuSequenceRepository _skuSequenceRepository;
+
+    public SkuGenerator(ISkuSequenceRepository skuSequenceRepository)
+    {
+        _skuSequenceRepository = skuSequenceRepository;
+    }
+
+    public async Task<string> GenerateSku(string companyCode, string categoryCode, string brandCode, CancellationToken cancellationToken)
+    {
+        var number = await _skuSequenceRepository.GetNextNumberForSkuAsync(companyCode, categoryCode, brandCode, cancellationToken);
+
+        return FormatSku(companyCode, categoryCode, brandCode, number);
+    }
+
+    private string FormatSku(string company, string category, string brand, int number)
+        => $"{company}-{category}-{brand}-{number:D5}";
+}
